a454ce2e139e22ae9e5a37ab099e9be8453765de
varrascaeta <valen.arrascaeta@gmail.com>
2022-10-15 11:48:45 -0300

    Patient and Volunteer admins

--- fundation/person/admin.py
+++ fundation/person/admin.py
@@ -20,4 +20,61 @@ class PersonAdmin(admin.ModelAdmin):
         "phone_number"
     ]
 
-# Register your models here.
+    
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = (
+        "get_name",
+        "get_dni",
+        "get_care_centers",
+        "health_insurance"
+    )
+    fields = [
+        "person"
+        "medication",
+        "health_insurance",
+        "care_taker"
+    ]
+
+    def get_name(self, obj):
+        return obj.person.name
+
+    def get_dni(self, obj):
+        return obj.person.dni
+
+    def get_care_centers(self, obj):
+        return "".join(obj.care_centers.all())
+
+    get_name.short_description = "Name"
+    get_dni.short_description = "DNI"
+    get_care_centers.short_description = "Care Centers"
+
+
+@admin.register(Volunteer)
+class VolunteerAdmin(admin.ModelAdmin):
+    list_display = (
+        "get_name",
+        "get_dni",
+    )
+    fields = [
+        "person",
+    ]
+
+    def get_name(self, obj):
+        return obj.person.name
+
+    def get_dni(self, obj):
+        return obj.person.dni
+
+    def get_care_centers(self, obj):
+        return "".join(obj.care_centers.all())
+
+    get_name.short_description = "Name"
+    get_dni.short_description = "DNI"
+    get_care_centers.short_description = "Care Centers"
+
+
+@admin.register(HealthInsurance)
+class HealthInsuranceAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    fields = ["name"]
