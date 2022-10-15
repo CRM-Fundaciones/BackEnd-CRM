0c7c746f0223223f147a1a727802314c7b1f8fcb
varrascaeta <valen.arrascaeta@gmail.com>
2022-10-15 11:48:19 -0300

    Person Admin

--- fundation/person/admin.py
+++ fundation/person/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from person.models import HealthInsurance, Person, Patient, Volunteer
+
+admin.autodiscover()
+admin.site.enable_nav_sidebar = False
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("name", "dni", "address", "email")
+    search_fields = [
+        "name",
+        "dni"
+    ]
+    fields = [
+        "name",
+        "dni",
+        "email",
+        "address",
+        "phone_number"
+    ]
 
 # Register your models here.
