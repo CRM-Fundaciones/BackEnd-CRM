f65a48d81b65f90737ee790d1d2ef89f569baec9
varrascaeta <valen.arrascaeta@gmail.com>
2022-10-15 12:04:16 -0300

    Institution admin

--- fundation/institution/admin.py
+++ fundation/institution/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from institution.models import Contract, Institution
+
+class ContractInline(admin.TabularInline):
+    model = Contract
+    fields = ["start_time", "end_time"]
+
+
+@admin.register(Institution)
+class InstitutionAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    fields = ["name"]
+    inlines = [ContractInline]
+    def get_contracts(self, obj):
+        return 
+
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    list_display = ["start_time", "end_time"]
+    fields = ["institution", "start_time", "end_time"]
